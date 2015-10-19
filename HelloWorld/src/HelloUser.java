3248e916ca86bcd70995ef3d5724a8c436082705
Aurel-Roci <aurelroci@hotmail.com>
2015-10-19 21:33:29 +0200

    testing

--- HelloWorld/src/HelloUser.java
+++ HelloWorld/src/HelloUser.java
@@ -1,13 +1,4 @@
 
 public class HelloUser {
-	private String userName;
- 
 
-	public void setUser(String user) {
-		userName = user;
-	}
-
-	public void greetUser() {
-		System.out.println("Hello " + userName+"!");
-	}
 }
